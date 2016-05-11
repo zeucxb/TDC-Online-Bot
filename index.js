db8a08d0322344944af503921a2a74730c057562
Eliseu Benedito Codinhoto <zeucxb@gmail.com>
2016-05-11 17:57:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,26 @@ var driver = new webdriver.Builder().forBrowser('firefox').build();
 
 driver.manage().window().maximize();
 
-// Coloque emails obvios
-for(var i = 1900; i <= 3000; i++) {
-	var email = i + '@gmail.com';
-
+function bot(email) {
 	driver.get('http://tdconline.com.br/?bv=5786487618535424');
 
 	driver.findElement(By.name('name')).sendKeys('hack ' + i);
 	driver.findElement(By.name('email')).sendKeys(email);
 	driver.findElement(By.tagName('button')).click();
+}
+
+var email;
+
+for(var i = 1900; i <= 3000; i++) {
+	email = i + '@hotmail.com';
+
+	bot(email);
+
+	email = i + '@gmail.com';
+
+	bot(email);
+
+	email = i + 'yahoo.com';
+
+	bot(email);
 }
